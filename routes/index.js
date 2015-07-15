99afcadf833dd50fe2bdf70af8c559ffc74d36b2
Son Truong <sont85@gmail.com>
2015-07-15 11:23:23 -0700

    merged with elijah dependency, twit and mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+var Twit = require('twit')
+
+
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
