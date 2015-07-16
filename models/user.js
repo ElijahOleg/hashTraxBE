0aaa5588b11f7aa3a51b1efed19ac1977cabbf20
Trey Huffine <treyhuffine@gmail.com>
2015-07-16 00:40:08 -0700

    Change user schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  userHash: String,
   searchTerms: [{
     term: { type: String, required: true },
     tweetIds: [ { type: mongoose.Schema.ObjectId, ref: 'Tweet' } ]
